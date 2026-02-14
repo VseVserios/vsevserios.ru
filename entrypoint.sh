1cd92d48b9c649280a1a9f58501ca3f99882fec2
John Doe <lefortovos@proton.me>
2026-02-14 11:55:45 +0300

    fff: docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 set -e
 
+if [ -n "$DATABASE_URL" ]; then
+python - <<'PY'
+import os
+import time
+
+import psycopg
+
+dsn = os.environ.get("DATABASE_URL")
+deadline = time.time() + 60
+
+while True:
+    try:
+        with psycopg.connect(dsn, connect_timeout=3) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT 1")
+        break
+    except Exception as e:
+        if time.time() > deadline:
+            raise
+        time.sleep(1)
+PY
+fi
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
