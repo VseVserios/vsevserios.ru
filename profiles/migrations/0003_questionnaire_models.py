597c5760e40a479b2750894273a08c87abc29bab
John Doe <lefortovos@proton.me>
2026-02-14 12:12:06 +0300

    ch1

--- profiles/migrations/0003_questionnaire_models.py
+++ profiles/migrations/0003_questionnaire_models.py
@@ -102,7 +102,7 @@ class Migration(migrations.Migration):
                     ),
                 ),
                 ("value", models.CharField(max_length=64)),
-                ("label", models.CharField(max_length=128)),
+                ("label", models.CharField(max_length=256)),
                 ("order", models.PositiveIntegerField(default=0)),
                 (
                     "question",
