07db781aabe3cf5206009009612a684b26777945
John Doe <lefortovos@proton.me>
2026-03-09 21:33:46 +0300

    yandex

--- config/urls.py
+++ config/urls.py
@@ -2,15 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.http import HttpResponse
-from django.urls import include, path
+from django.http import Http404, HttpResponse
+from django.urls import include, path, re_path
 
 from config.sitemaps import PublicProfilesSitemap, StaticViewSitemap
 from matchmaking.views import landing, robots_txt
 
 
-def yandex_webmaster_verification(request):
-    content = (settings.BASE_DIR / "yandex_9ea13c1b28a1285c.html").read_text(encoding="utf-8")
+def yandex_webmaster_verification(request, hash: str):
+    filename = f"yandex_{hash}.html"
+    file_path = settings.BASE_DIR / filename
+    if not file_path.exists():
+        raise Http404
+    content = file_path.read_text(encoding="utf-8")
     return HttpResponse(content, content_type="text/html; charset=utf-8")
 
 
@@ -26,8 +30,8 @@ sitemaps = {
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("panel/", include("panel.urls")),
-    path(
-        "yandex_9ea13c1b28a1285c.html",
+    re_path(
+        r"^yandex_(?P<hash>[0-9a-f]{16})\.html$",
         yandex_webmaster_verification,
         name="yandex_webmaster_verification",
     ),
